7277a8d9ef9a31291091a7c71e31bd7e7af8801a
lx546956 <lx546956@antgroup.com>
2026-08-13 19:45:16 +0800

    1

--- meteor_run/run.sh
+++ meteor_run/run.sh
@@ -1,1 +1,1 @@
-nohup bash experiments_scripts/opd-baseline-deepseek-r1-distill-qwen-1.5b-justrl-deepseek-1.5b.sh > meteor_run/opd-baseline-deepseek-r1-distill-qwen-1.5b-justrl-deepseek-1.5b.log 2>&1 &
+nohup bash experiments_scripts/opd-plus-attention.sh > meteor_run/opd-plus-attention.log 2>&1 &
