ce9e08efdab0d257ca58c119fb23771de022468d
lx546956 <lx546956@antgroup.com>
2026-09-10 18:39:40 +0800

    1

--- meteor_run/run.sh
+++ meteor_run/run.sh
@@ -1,1 +1,1 @@
-nohup bash experiments_scripts/opd-standard-qwen3-4b-nonthinking-rl-math-step500.sh > meteor_run/opd-standard-qwen3-4b-nonthinking-rl-math-step500.log 2>&1 &
+nohup bash experiments_scripts/etopd-ratio-qwen3-4b-nonthinking-rl-math-step500.sh > meteor_run/etopd-ratio-qwen3-4b-nonthinking-rl-math-step500.log 2>&1 &
